1287df77a5ae14796e212621cba11853cd6cf27f
Misso <missoxd@gmail.com>
2018-09-22 11:05:46 -0300

    iniciando o graphql

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const express_graphql = require('express-graphql')
+const { buildSchema } = require('graphql');
 
 const app = express()
-
 app.use(bodyParser.json())
+
+var root = {
+    message: () => 'Hello World!'
+}
+
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`)
+
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(3000, () => {
 	console.log('Example app listening on port 3000!')
