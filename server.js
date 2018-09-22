0de5d9ce1e5671d78c65b767f9550f9cc14ab80b
Misso <missoxd@gmail.com>
2018-09-22 11:42:51 -0300

    wip

--- server.js
+++ server.js
@@ -6,15 +6,62 @@ const { buildSchema } = require('graphql');
 const app = express()
 app.use(bodyParser.json())
 
-var root = {
-    message: () => 'Hello World!'
-}
-
 var schema = buildSchema(`
     type Query {
-        message: String
+        agency(id: Int!): Agency
+        agencies: [Agency]
+    },
+    type Agency {
+        id: Int
+        name: String,
+        medias: [Media]
+    },
+    type Media {
+        id: Int
+        title: String
+        description: String
+        url: String
+        type: String
+        slug: String
+        comments: [Comment]
+    },
+    type Comment {
+        id: Int
+        comment: String
+        name: String
     }
-`)
+`);
+
+var data = [
+    {
+        id: 1,
+        name: 'Arpina',
+        medias: [
+            {
+                id: 1,
+                title: 'teste1',
+                description: 'teste2'
+            }
+        ]
+    }
+]
+
+var getAgency = function(args) { 
+    var id = args.id;
+    return data.filter(course => {
+        return course.id == id;
+    })[0];
+}
+
+var getAgencies = function(args) {
+    return data;
+}
+
+var root = {
+    agency: getAgency,
+    agencies: getAgencies
+};
+
 
 app.use('/graphql', express_graphql({
     schema: schema,
